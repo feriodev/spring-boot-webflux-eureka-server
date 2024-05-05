a97203c363132caa7f25f5ad1430194591521c35
Fernando Cuadros <daniel.cuadros@teamsoft.com.pe>
2024-05-05 17:00:56 -0500

    Actualizacion de repositorio

---
+++ src/test/java/com/pe/feriocuadros/springboot/webflux/SpringBootWebfluxEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.feriocuadros.springboot.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
